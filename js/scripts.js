d4ff8fe62d1649b2d67188efcd90246688bad98b
Alex Skreen and Benjamin Thom <alex@>
2020-01-23 11:42:28 -0800

    add leapYear function and passed test

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
-$(document).ready(function(){
+// back-end
 
+var leapYear = function(year) {
+  return false;
+};
+
+// front-end logic
+
+$(document).ready(function() {
+  $("form#leap-year").submit(function (event) {
+    event.preventDefault();
+
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+
+  });
 });
